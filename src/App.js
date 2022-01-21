9d558ea452c825565b90e4f42f0283bcbb331a71
mcodemax <mwalterjohnson7@gmail.com>
2022-01-20 22:45:31 -0800

    add foodyuhApi f()s for calls to fdcapi in backend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,89 @@
 import logo from './logo.svg';
 import './App.css';
+import jwt from "jsonwebtoken";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
+import useLocalStorage from "./hooks/useLocalStorage";
+// import Navigation from "./routes-nav/Navigation";
+// import Routes from "./routes-nav/Routes";
+import FoodyuhApi from './foodyuhApi';
 
+
+
+// Key name for storing token in localStorage for "remember me" re-login
+export const TOKEN_STORAGE_ID = "foodyuh-token";
+
+//make api call back end for token, store token as state
 function App() {
+  console.log(FoodyuhApi.getImages('fight78t67gbyv7658').then(e => console.log(e)))
+
+  // const [infoLoaded, setInfoLoaded] = useState(false);
+  // const [currentUser, setCurrentUser] = useState(null);
+  // // ^store credentials in state
+  // const [jobIds, setJobIds] = useState([]);
+  // const [token, setToken] = useLocalStorage(TOKEN_STORAGE_ID);
+
+  // console.debug(
+  //   "App",
+  //   "infoLoaded=", infoLoaded,
+  //   "currentUser=", currentUser,
+  //   "token=", token,
+  // );
+
+  // /** Handles site-wide login.
+  //  *
+  //  * Make sure you await this function and check its return value!
+  //  */
+  // async function login(username, password) {
+  //   try {
+  //     let token = await //JoblyApi.login(username, password);
+  //     setToken(token);
+  //     return { success: true };
+  //   } catch (errors) {
+  //     console.error("login failed", errors);
+  //     return { success: false, errors };
+  //   }
+  // }
+
+  //  // Load user info from API. Until a user is logged in and they have a token,
+  // // this should not run. It only needs to re-run when a user logs out, so
+  // // the value of the token is a dependency for this effect.
+
+  
+  // useEffect(function loadUserInfo() {
+  //   console.debug("App useEffect loadUserInfo", "token=", token);
+
+  //   async function getCurrentUser() {
+  //     if (token) {
+  //       try {
+  //         let { username } = jwt.decode(token);
+  //         // put the token on the Api class so it can use it to call the API.
+  //         FoodyuhApi.token = token;
+  //         let currentUser = await FoodyuhApi.getCurrentUser(username);
+  //         setCurrentUser(currentUser);
+  //         setApplicationIds(new Set(currentUser.applications));// turn this into plate ids and 
+  //         //possibly fdcIds
+  //       } catch (err) {
+  //         console.error("App loadUserInfo: problem loading", err);
+  //         setCurrentUser(null);
+  //       }
+  //     }
+  //     setInfoLoaded(true);
+  //   }
+
+  //   // set infoLoaded to false while async getCurrentUser runs; once the
+  //   // data is fetched (or even if an error happens!), this will be set back
+  //   // to false to control the spinner.
+  //   setInfoLoaded(false);
+  //   getCurrentUser();
+  // }, [token]);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <Navigation logout={logout} />
+        <Routes login={login} signup={signup} /> */}
       </header>
     </div>
   );
---
+++ src/foodyuhApi.js
@@ -0,0 +1,123 @@
+import axios from "axios";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+
+/** API Class.
+ *
+ * Static class tying together methods used to get/send to to the API.
+ * There shouldn't be any frontend-specific stuff here, and there shouldn't
+ * be any API-aware stuff elsewhere in the frontend.
+ *
+ */
+
+class FoodyuhApi {
+  // the token for interactive with the API will be stored here.
+  static token;
+
+  static async request(endpoint, data = {}, method = "get") {
+    console.debug("API Call:", endpoint, data, method);
+
+    //there are multiple ways to pass an authorization token, this is how you pass it in the header.
+    //this has been provided to show you another way to pass the token. you are only expected to read this code for this project.
+    const url = `${BASE_URL}/${endpoint}`;
+    const headers = { Authorization: `Bearer ${FoodyuhApi.token}` };
+    const params = (method === "get")
+        ? data
+        : {};
+        
+    try {
+      return (await axios({ url, method, data, params, headers })).data;
+    } catch (err) {
+      console.error("API Error:", err.response);
+      let message = err.response.data.error.message;
+      throw Array.isArray(message) ? message : [message];
+    }
+  }
+
+  // Individual API routes
+
+  /** Get details on a company by handle. */
+
+//   static async getCompany(handle) {
+//     let res = await this.request(`companies/${handle}`);
+//     return res.company;
+//   }
+
+  /** Get auth token or get denied; works */
+  static async login(username, password) {
+    let res = await this.request(`auth/token`, {
+      username: username,
+      password: password
+    }, 'post');
+    
+    return res.token;
+  }
+
+  /** get list of all companies (filtered by name if not undefined) */
+//   static async getAllCompanies(name) {
+//     let res = await this.request(`companies`, {name});
+//     return res.companies;
+//   }
+
+  /** get list of all Jobs (filtered by name if not undefined) */
+//   static async getAllJobs(title) {
+//     let res = await this.request(`jobs`, {title});
+//     return res.jobs;
+//   }
+
+  /** Signup and return token; works */
+  static async register(userObj) {
+    let res = await this.request(`auth/register`, userObj, 'post');
+    return res.token;
+  }
+
+
+  /** Get the current user.; works */
+  static async getCurrentUser(username) {
+    let res = await this.request(`users/${username}`);
+    return res.user;
+  }
+
+  //GET requests don't have a request body
+
+  /** Get multiple fdcapi generated food matches; works  */
+  static async searchForFoods(search){
+    let res = await this.request(`fdcapi/foods/${search}`);
+    return res.foodRes;
+  }
+
+  /** Get info on a single food by its fdcId; works */
+  static async getFoodbyFdcId(fdcId){
+    let res = await this.request(`fdcapi/fdcid/${fdcId}`);
+    return res.food;
+  }
+
+  /** Get images of foods; works */
+  static async getImages(search){
+    let res = await this.request(`fdcapi/pexels/${search}`);
+    return res.response.photos;
+  }//in front end use a photo not avail if res is empty
+
+  /** Alert current loggedin user's info.
+   * User can only change { firstName, lastName, password, email } = infoObj
+   */
+  static async editUserInfo(username, infoObj) {
+    let res = await this.request(`users/${username}`, infoObj, 'patch');
+    return res.user;
+  }
+
+  /** Applies for jobs
+   * I: (username, id), id can be a string prob
+   * O: returns jobId
+   */
+//   static async jobApply(username, id){
+//     console.debug(`${username}/jobs/${id}`);
+//     let res = await this.request(`users/${username}/jobs/${id}`, {}, 'post');
+//     return res.applied;
+//   }
+}
+
+// for now, put token ("testuser" / "password" on class)
+FoodyuhApi.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlzUGFpZCI6ZmFsc2UsImlhdCI6MTY0MjY2MDc0Mn0.aaeUXw_es2gWJc1Dveb9aNBVYzSCtWNANPTIN9MOODY"
+
+export default FoodyuhApi;
